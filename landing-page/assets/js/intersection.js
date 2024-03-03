0b76998d304dd1fdb381145d968bfac47b2e5b35
Joseph Ehigiator <joeehis1@gmail.com>
2024-03-03 13:23:32 -0500

    Landing page ready pending integration with other pages and application

--- landing-page/assets/js/intersection.js
+++ landing-page/assets/js/intersection.js
@@ -3,7 +3,7 @@ const mainHeader = document.querySelector("#main-header");
 const currentScroll = window.scrollY;
 
 window.addEventListener("scroll", (e) => {
-    if (window.scrollY > mainHeader.offsetHeight) {
+    if (window.scrollY > 0) {
         mainHeader.classList.add("add-highlight");
     } else {
         mainHeader.classList.remove("add-highlight");
